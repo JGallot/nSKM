afc14180f59c0c6eb8fb099881c89253ac8b70f8
JGallot <jerome.gallot@normandie-univ.fr>
2015-05-19 11:09:22 +0200

    Upgrade script corrected (close #1)

--- install/upgrades/SKM_2.5_to_nSKM_0.1.0.sql
+++ install/upgrades/SKM_2.5_to_nSKM_0.1.0.sql
@@ -1,6 +1,11 @@
-CREATE TABLE `config` IF NOT EXISTS (
-  `key` varchar(30) NOT NULL,
-  `val` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1$$
+# Create Config Table
+
+CREATE TABLE IF NOT EXISTS config (
+key varchar(30) NOT NULL,
+val varchar(50) NOT NULL
+) ENGINE=InnoDB;
+
+# Add version
+INSERT INTO config values ('version','O.1.0');
 
 
