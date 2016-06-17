367637eb94968b586c2ecefe483b8b045fd5f680
JGallot <jerome.gallot@normandie-univ.fr>
2016-06-17 20:13:28 +0200

    Correction MYSQL Upgrade

--- install/upgrades/SKM_2.5_to_nSKM_0.1.0.sql
+++ install/upgrades/SKM_2.5_to_nSKM_0.1.0.sql
@@ -1,11 +1,10 @@
 # Create Config Table
 
-CREATE TABLE IF NOT EXISTS config (
-key varchar(30) NOT NULL,
-val varchar(50) NOT NULL
+CREATE TABLE IF NOT EXISTS `config` (
+`key` varchar(30) NOT NULL,
+`val` varchar(50) NOT NULL
 ) ENGINE=InnoDB;
 
 # Add version
 INSERT INTO config values ('version','O.1.0');
-
 
