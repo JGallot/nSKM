24e3c8c2620f607dc960df2e6cabe9de69faeecd
Jerome Gallot <jerome.gallot@unicaen.fr>
2017-08-29 15:28:02 +0200

    Add MySQL port in configuration

--- inc/database.inc.php
+++ inc/database.inc.php
@@ -1,2 +1,8 @@
-<?php $mysql_link = mysql_connect($mysql_host, $mysql_user, $mysql_pass);
-mysql_select_db($mysql_db) or die("Could not select database");?>
+<?php
+// Todo add error control
+$mysql_link = mysql_connect("$mysql_host:$mysql_port", $mysql_user, $mysql_pass);
+
+mysql_select_db($mysql_db) or die("Could not select database : $mysql_db");
+
+
+?>
